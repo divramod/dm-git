d9220b73d15f2a8a09906cb774c6a02485b95347
divramod <divramod@gmail.de>
2015-08-09 11:26:02 +0200

    cloneGithubrepository adpated to use getConfigFile

--- jobs/cloneGithubUserRepository/index.js
+++ jobs/cloneGithubUserRepository/index.js
@@ -1,6 +1,7 @@
 // =========== [ REQUIRE ] ===========
 var co = require("co");
 var fs = require('fs');
+var getConfigFile = require("./../../tasks/getConfigFile/index.js").start;
 require("shelljs/global");
 
 // =========== [ MODULE DEFINE ] ===========
@@ -9,37 +10,30 @@ var job = {};
 // =========== [ job.start() ] ===========
 job.start = co.wrap(function*() {
     try {
-        console.log("start cloneGithubUserRepository");
         var argv3 = process.argv[3];
         var configFilePath = env["HOME"] + '/.dm-git.json';
-        if (test("-f", configFilePath)) {
-            var config = JSON.parse(fs.readFileSync(configFilePath, 'utf8'));
-            if (config) {
-                if (config.cloneGithubUserRepository.user) {
-                    var user = config.cloneGithubUserRepository.user;
-                    if (user && argv3) {
-                        if (argv3) {
-                            var repository = argv3;
-                        } else {
-
-                            var repositoryAnswer =
-                                yield dmPrompt({
-                                    type: "input",
-                                    name: "repository",
-                                    message: "What is your repository name:"
-                                });
-                            var repository = repositoryAnswer.repository;
-                        }
-                        exec('git clone https://github.com/' + user + '/' + repository + '.git', {
-                            silent: false
-                        });
+        var config = getConfigFile().cloneGithubUserRepository;
+        if (config) {
+            if (config.user) {
+                var user = config.user;
+                if (user && argv3) {
+                    if (argv3) {
+                        var repository = argv3;
+                    } else {
+                        var repositoryAnswer =
+                            yield dmPrompt({
+                                type: "input",
+                                name: "repository",
+                                message: "What is your repository name:"
+                            });
+                        var repository = repositoryAnswer.repository;
                     }
+                    exec('git clone https://github.com/' + user + '/' + repository + '.git', {
+                        silent: false
+                    });
                 }
             }
         }
-
-
-
     } catch (e) {
         console.log("Filename: ", __filename, "\n", e.stack);
     }
